a19247e90df08f2ec97cc601d98dce46a575e1bc
tiagob01 <tiagobrett70@gmail.com>
2024-10-09 17:27:32 +0100

    fixed brake

--- Car.py
+++ Car.py
@@ -9,7 +9,8 @@ class Car:
         self.speed += 5
 
     def brake(self):
-        self.speed -= 5
+        if self.speed > 0:
+            self.speed -= 5
 
     def step(self):
         self.odometer += self.speed
